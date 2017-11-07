a21026e27e816f7b2bdb58b337520a5cee5b29bb
Will Fancher <elvishjerricco@gmail.com>
2017-11-07 10:19:02 -0500

    Add demo android app

--- default.nix
+++ default.nix
@@ -7,6 +7,12 @@
     frontend = ./frontend;
   };
 
+  android.frontend = {
+    executableName = "frontend";
+    applicationId = "org.example.frontend";
+    displayName = "Example Android App";
+  };
+
   shells = {
     ghc = ["common" "backend" "frontend"];
     ghcjs = ["common" "frontend"];
